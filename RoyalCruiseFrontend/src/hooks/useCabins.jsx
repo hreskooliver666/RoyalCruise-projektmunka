c84ddd26c0e05c2d77bc0a8305bf62c1736dbf5a
ropika <21c_hreskoo@nyirszikszi.hu>
2026-04-26 17:33:29 +0200

    Creating hooks folder and adding custom hooks

---
+++ RoyalCruiseFrontend/src/hooks/useCabins.jsx
@@ -0,0 +1,8 @@
+﻿// Ez a fájl egy kényelmi hookot ad, amely biztonságosan kiolvassa a kabin context aktuális értékét.
+import { useContext } from "react";
+import CabinsContext from "../contexts/CabinsContext.jsx";
+
+// A useCabins hook visszaadja a CabinsContext értékét, és hibát dob, ha provideren kívül használják.
+export default function useCabins() {
+  return useContext(CabinsContext);
+}
